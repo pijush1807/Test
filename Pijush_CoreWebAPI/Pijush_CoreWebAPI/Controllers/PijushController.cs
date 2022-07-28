4a48ef9132ebabc621fe809a893bdce9a736ae65
Pijush <pijush1807@outlook.com>
2022-07-28 20:53:34 +0530

    adding first file on the repository Pijush_corewebapi

---
+++ Pijush_CoreWebAPI/Pijush_CoreWebAPI/Controllers/PijushController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pijush_CoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PijushController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Harsh sir,Get method run successfully";
+        }
+        [HttpPost]
+        public string Post(string? name)
+        {
+            return "Hello Harsh sir, I am " + name + " Saved my name";
+        }
+    }
+}
